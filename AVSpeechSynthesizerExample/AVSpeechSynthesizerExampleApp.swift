956a52da997cf4b1ce7269387ff38b77dd119736
Hoye Lam <hoyelam@protonmail.com>
2022-08-27 15:17:49 +0200

    Initial Commit

---
+++ AVSpeechSynthesizerExample/AVSpeechSynthesizerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AVSpeechSynthesizerExampleApp.swift
+//  AVSpeechSynthesizerExample
+//
+//  Created by Hoye Lam on 27/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct AVSpeechSynthesizerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
